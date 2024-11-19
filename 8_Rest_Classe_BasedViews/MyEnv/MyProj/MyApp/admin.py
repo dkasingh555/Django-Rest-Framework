733a085b3f77c4079a5c79ec3b381c756eac5527
dkasingh555 <dkasingh555@gmail.com>
2024-11-19 14:58:56 +0530

    Adding Review In Code

--- 8_Rest_Classe_BasedViews/MyEnv/MyProj/MyApp/admin.py
+++ 8_Rest_Classe_BasedViews/MyEnv/MyProj/MyApp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Carlist,Studentlist,Schoollist
+from .models import Carlist,Studentlist,Schoollist,Review
 
 
 # Register your models here.
 admin.site.register(Carlist)
 admin.site.register(Studentlist)
 admin.site.register(Schoollist)
+admin.site.register(Review)
--- 8_Rest_Classe_BasedViews/MyEnv/MyProj/MyApp/models.py
+++ 8_Rest_Classe_BasedViews/MyEnv/MyProj/MyApp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class Carlist(models.Model):
     name=models.CharField(max_length=50)
@@ -21,3 +23,13 @@ class Schoollist(models.Model):
     
     def __str__(self):
         return self.School
+
+
+class Review(models.Model):
+    rating=models.IntegerField(validators=[MaxValueValidator,MinValueValidator])
+    comments=models.CharField(max_length=200,null=True)
+    student=models.ForeignKey(Studentlist,on_delete=models.CASCADE,related_name="Reviews",null=True)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+    def __str__(self) -> str:
+        return "The Rating Of "+ self.student.Stu_Name +":---"+str(self.rating)
